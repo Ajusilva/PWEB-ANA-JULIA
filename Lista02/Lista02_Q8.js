09a63f64cc92efbe88790dfdabad27f35444e5eb
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:06:13 -0300

    Update Lista02_Q8.js

--- Lista02/Lista02_Q8.js
+++ Lista02/Lista02_Q8.js
@@ -1,1 +1,15 @@
+let quantidade = 0;
+let soma = 0;
 
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 !== 0) {
+    continue;// pesquisei como usa esse bixo no gemini, pq nao sabia o que era
+  }
+
+  console.log(i);
+  quantidade++;
+  soma = soma + i;
+}
+
+console.log("Quantidade de múltiplos:", quantidade);
+console.log("Soma de todos eles:", soma);
