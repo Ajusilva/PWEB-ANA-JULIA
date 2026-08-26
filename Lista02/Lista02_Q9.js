c4d2b8893cf40bdf4c1b0995a60e3f7bdcb89c06
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:06:54 -0300

    Update Lista02_Q9.js

--- Lista02/Lista02_Q9.js
+++ Lista02/Lista02_Q9.js
@@ -1,1 +1,15 @@
+const notas = [8, 6.5, 9, 7.5];
+let soma = 0;
 
+for (let i = 0; i < notas.length; i++) {
+  soma = soma + notas[i];
+}
+
+let media = soma / notas.length;
+console.log("Média:", media);
+
+if (media >= 7) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
