796abe85568781b28deecec9b450b96e6c0e7235
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:04:44 -0300

    Update Lista02_Q6.js

--- Lista02/Lista02_Q6.js
+++ Lista02/Lista02_Q6.js
@@ -1,1 +1,27 @@
+let numero = 6;
+let operacao = "+";
 
+switch (operacao) {
+  case "+":
+    for (let i = 1; i <= 10; i++) {
+      console.log(numero + " + " + i + " = " + (numero + i));
+    }
+    break;
+  case "-":
+    for (let i = 1; i <= 10; i++) {
+      console.log(numero + " - " + i + " = " + (numero - i));
+    }
+    break;
+  case "*":
+    for (let i = 1; i <= 10; i++) {
+      console.log(numero + " * " + i + " = " + (numero * i));
+    }
+    break;
+  case "/":
+    for (let i = 1; i <= 10; i++) {
+      console.log(numero + " / " + i + " = " + (numero / i));
+    }
+    break;
+  default:
+    console.log("Operação inválida");
+}
