5139c487d0fa292601e0a5c1448d30358a4e2599
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:03:30 -0300

    Update Lista02_Q5.js

--- Lista02/Lista02_Q5.js
+++ Lista02/Lista02_Q5.js
@@ -1,1 +1,20 @@
+const a = 10;
+const b = 4;
+const operacao = "*";
 
+switch (operacao) {
+  case "+":
+    console.log(a + " + " + b + " = " + (a + b));
+    break;
+  case "-":
+    console.log(a + " - " + b + " = " + (a - b));
+    break;
+  case "*":
+    console.log(a + " * " + b + " = " + (a * b));
+    break;
+  case "/":
+    console.log(a + " / " + b + " = " + (a / b));
+    break;
+  default:
+    console.log("Operação inválida");
+}
