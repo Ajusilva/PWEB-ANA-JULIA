e4d7ab2a847bacc7f00e0c20b9676e41587b5ab1
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 10:43:59 -0300

    Rename exercicio15.js to Lista00/exercicio15.js

---
+++ Lista00/exercicio15.js
@@ -0,0 +1,4 @@
+produto = "Caderno"
+preco = 12.5
+
+console.log(`O produto ${produto} custa R$ ${preco}.`)
--- exercicio15.js
+++
@@ -1,4 +0,0 @@
-produto = "Caderno"
-preco = 12.5
-
-console.log(`O produto ${produto} custa R$ ${preco}.`)
