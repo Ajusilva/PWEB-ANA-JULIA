83da607f35691f63b4a58e1e7457190cfb15e776
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:05:14 -0300

    Update Lista02_Q7.js

--- Lista02/Lista02_Q7.js
+++ Lista02/Lista02_Q7.js
@@ -1,1 +1,20 @@
+let totalPares = 0;
+let totalImpares = 0;
 
+for (let i = 1; i <= 20; i++) {
+  let resto = i % 2;
+
+  switch (resto) {
+    case 0:
+      console.log(i + " é par");
+      totalPares++;
+      break;
+    case 1:
+      console.log(i + " é ímpar");
+      totalImpares++;
+      break;
+  }
+}
+
+console.log("Pares encontrados:", totalPares);
+console.log("Ímpares encontrados:", totalImpares);
