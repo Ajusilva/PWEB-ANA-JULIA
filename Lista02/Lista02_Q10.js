21c58d8f8ff8bd49a62f6953bff6d8143c0de309
Ajusilva <juliasilvatds10@gmail.com>
2026-08-26 11:08:07 -0300

    Update Lista02_Q10.js

--- Lista02/Lista02_Q10.js
+++ Lista02/Lista02_Q10.js
@@ -1,4 +1,32 @@
-for (let i = 10; i >= 1; i--) {
-  console.log(i);
-}
-console.log("Fim!");
+
+const entradasDoUsuario = [1, 15, 1, 25, 2, 99, 0];
+
+let total = 0;
+let opcao;
+let i = 0;
+
+do {
+  opcao = entradasDoUsuario[i];
+  i++;
+
+  switch (opcao) {
+    case 1:
+      let valor = entradasDoUsuario[i];
+      i++;
+      total = total + valor;
+      console.log("Somado " + valor + " ao total.");
+      break;
+
+    case 2:
+      console.log("Total acumulado: " + total);
+      break;
+
+    case 0:
+      console.log("Saindo do programa...");
+      break;
+
+    default:
+      console.log("Opção inválida: " + opcao);
+  }
+
+} while (opcao !== 0);
